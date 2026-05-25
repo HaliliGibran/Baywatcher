d0a0024188465ca7b39e04b75ded8212e88ea61b
ilun <vanchaud489@gmail.com>
2026-05-25 07:26:14 -0400

    解耦image_handle：引入runtime_feedback解除循环包含

--- lq_2k0301_project_template_for_running/Code/User/image/image_handle.cc
+++ lq_2k0301_project_template_for_running/Code/User/image/image_handle.cc
@@ -2,8 +2,7 @@
 #include "image_data.h"
 #include "image_math.h"
 #include "image_midline_process.h"
-#include "Encoder.h"
-#include "PID.h"
+#include "runtime_feedback.h"
 #include "transform_table.h"
 #include <cmath>
 
@@ -77,13 +76,14 @@ static int preview_shift_from_speed_feedback()
         return 0;
     }
 
-    const float target_speed = std::fabs(PID.base_target_speed);
+    const float target_speed = std::fabs(BayWatcher_GetBaseTargetSpeed());
     if (target_speed <= 1e-3f)
     {
         return 0;
     }
 
-    float actual_speed = (vL + vR) * 0.5f;
+    float actual_speed =
+        (BayWatcher_GetLeftWheelSpeed() + BayWatcher_GetRightWheelSpeed()) * 0.5f;
     if (actual_speed < 0.0f)
     {
         actual_speed = 0.0f;
---
+++ lq_2k0301_project_template_for_running/Code/User/inc/runtime_feedback.h
@@ -0,0 +1,8 @@
+#ifndef _BAYWATCHER_RUNTIME_FEEDBACK_H_
+#define _BAYWATCHER_RUNTIME_FEEDBACK_H_
+
+float BayWatcher_GetBaseTargetSpeed(void);
+float BayWatcher_GetLeftWheelSpeed(void);
+float BayWatcher_GetRightWheelSpeed(void);
+
+#endif
